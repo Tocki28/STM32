91ace48cb5c1e83a4d5f844c7c612d7166a32876
Tocki28 <138053880+Tocki28@users.noreply.github.com>
2023-08-02 11:35:37 +0300

    Add files via upload

---
+++ PID/PID.h
@@ -0,0 +1,59 @@
+/*
+ * PID.h
+ *
+ *  Created on: Jul 31, 2023
+ *      Author: Andrei
+ */
+
+#ifndef INC_PID_H_
+#define INC_PID_H_
+
+typedef struct
+{
+	/*
+	 * Constante de proportionalitate Controller:
+	 */
+	float K_Proportional;
+	float K_Integral;
+	float K_Derivative;
+
+	/*
+	 * Constanta de Timp a FTJ, de care avem nevoie pentru a atenua zgomotul introdus de termenul Derivative:
+	 */
+	float FTJ_Time_Constant;
+
+	/*
+	 * Limitele Iesirii Controllerului:
+	 */
+	float Min_Limit;
+	float Max_Limit;
+
+	/*
+	 * Limite Integrator:
+	 */
+	float Int_Min_Limit;
+	float Int_Max_Limit;
+
+	/*
+	 * Perioada de Esantionare:
+	 */
+	float Sample_Time;
+
+	/*
+	 * Date Controller:
+	 */
+	float Integrator;
+	float Prev_Error;
+	float Differentiator;
+	float Prev_Measurement;
+
+	/*
+	 * Output
+	 */
+	float Output;
+} PID_Controller;
+
+void PID_Initialise(PID_Controller *PID);
+float PID_Update(PID_Controller *PID,float Target,float Measurement);
+
+#endif /* INC_PID_H_ */
